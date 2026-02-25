bdd42efe3cbfb426a0fc6c23f8d0681d002626cf
Adam Shafer <shaferadam@gmail.com>
2026-02-25 15:52:13 -0500

    Added CreateView, UpdateView, DeleteView

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import get_user_model    #Adam Shafer
+from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse  # new
+
 from .models import Post
+
 
 class BlogTests(TestCase):
     @classmethod
@@ -44,3 +46,32 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "A good title")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_post_createview(self):  # new
+        response = self.client.post(
+            reverse("post_new"),
+            {
+                "title": "New title",
+                "body": "New text",
+                "author": self.user.id,
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "New title")
+        self.assertEqual(Post.objects.last().body, "New text")
+
+    def test_post_updateview(self):  # new
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {
+                "title": "Updated title",
+                "body": "Updated text",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated title")
+        self.assertEqual(Post.objects.last().body, "Updated text")
+
+    def test_post_deleteview(self):  # new
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Post
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request,"home.html",{'posts':posts})
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
 
-def post_detail(request,pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request,"post_detail.html",{"post":post})
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields=["title", "body"]
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
